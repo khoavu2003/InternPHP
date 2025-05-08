a72dc4b56036fc296149b7c88db17535f17181d9
Vu Dang Khoa <vukhang.51189@gmail.com>
2025-05-08 15:09:14 +0700

    dieu chinh production lan 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/product.js','resources/js/productAdd.js','resources/js/user.js','resources/js/customer.js'],
             refresh: true,
         }),
         tailwindcss(),
